d26a54890f6c954ea74a70e134221fd26c88a541
wiky-avis <peta-holding@yandex.ru>
2021-07-03 16:01:31 +0300

    установка админки

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 from app.config import Config
-from app.models import db
+from app.models import db, Tour
 
 app = Flask(__name__)
+admin = Admin(app)
 app.config.from_object(Config)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
 from app.views import *
+
+admin.add_view(ModelView(Tour, db.session))
